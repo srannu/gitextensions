9b11a4be593830772c09ec8a9a767d7ba864ee5a
ShanshanDu <nicoleflopy@gmail.com>
2015-11-16 21:32:54 +0800

    Increase version number

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -19,6 +19,6 @@
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.49.99.4")]
+[assembly: AssemblyVersion("2.49.99.5")]
 [assembly: AssemblyFileVersion("2.49.99")]
-[assembly: AssemblyInformationalVersion("2.49.99.4")]
+[assembly: AssemblyInformationalVersion("2.49.99.5")]
